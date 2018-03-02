2c03fc4ed4c63d2c3504e3bf2d830d7ed4a1f13e
Christophe Braud <christophe@agilitation.fr>
2018-03-02 17:24:00 +0100

    update config, new standard rules, fix all lint issues

--- test/config/test.js
+++ test/config/test.js
@@ -1,43 +1,42 @@
 const host = 'http://localhost:3000';
 
 module.exports = {
-    port: 3000,
-    host: host,
-    title: 'Test Assets',
-    campsi: {
-        mongo: {
-            'host': 'localhost',
-            'port': 27017,
-            'database': 'relationships'
-        }
-    },
-    services: {
-        trace: {
-            title: 'trace',
-        },
-        webHooks: {
-            title: 'WebHooks',
-            options: {
-                preserveHooks: false,
-                hooks: {
-                    wh1: {
-                        topic: 'webhooks/test/topic',
-                        callback: {
-                            url: 'http://localhost:3000/trace/my-wh1'
-                        }
-                    },
-                    wh2: {
-                        topic: 'webhooks/test/topic/other',
-                        callback: {
-                            method: 'POST',
-                            url: 'http://localhost:3000/trace/my-wh2?foo=bar',
-                            headers: {
-                                'x-foo': 'bar'
-                            }
-                        }
-                    }
-                }
-            }
-        }
+  port: 3000,
+  campsi: {
+    publicURL: host,
+    mongo: {
+      'host': 'localhost',
+      'port': 27017,
+      'database': 'relationships'
     }
+  },
+  services: {
+    trace: {
+      title: 'trace'
+    },
+    webHooks: {
+      title: 'WebHooks',
+      options: {
+        preserveHooks: false,
+        hooks: {
+          wh1: {
+            topic: 'webhooks/test/topic',
+            callback: {
+              url: 'http://localhost:3000/trace/my-wh1'
+            }
+          },
+          wh2: {
+            topic: 'webhooks/test/topic/other',
+            callback: {
+              method: 'POST',
+              url: 'http://localhost:3000/trace/my-wh2?foo=bar',
+              headers: {
+                'x-foo': 'bar'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 };
